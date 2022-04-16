27a58c99aee165ed0504b48c31a952fe846de7ac
Neothawreww <98629525+Neothawreww@users.noreply.github.com>
2022-04-16 02:00:17 -0500

    Update MyToken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract MyToken is ERC20, ERC20Detailed {
 
-    constructor(uint256 initialSupply) ERC20Detailed("Azure","AZE", 0) public {
+    constructor(uint256 initialSupply) ERC20Detailed("BitFuel","BFUEL", 0) public {
         _mint(msg.sender, initialSupply);
     }
 }
